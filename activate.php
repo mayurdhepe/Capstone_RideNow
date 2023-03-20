25272dcdf4aa2115e26c8e02ea0cefab364f5b3a
Mayur Dhepe <mayurdhepe@gmail.com>
2023-03-20 01:32:17 -0400

    changes for activate pages

--- activate.php
+++ activate.php
@@ -105,6 +105,262 @@ include('userDao.php');
             </div>
         </div>
     </div>
+    
+    <form method="post" id="loginform">
+    <div class="modal" id="loginModal" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h4 id="myModalLabel">
+              Rider Login
+            </h4>
+          </div>
+          <div class="modal-body">
+            <div id="loginmessage"></div>
+            <div class="form-group">
+              <label for="loginemail" class="sr-only">Email:</label>
+              <input class="form-control" type="email" name="loginemail" id="loginemail" placeholder="Email"
+                maxlength="50">
+            </div>
+            <div class="form-group">
+              <label for="loginpassword" class="sr-only">Password</label>
+              <input class="form-control" type="password" name="loginpassword" id="loginpassword" placeholder="Password"
+                maxlength="30">
+            </div>
+            <div class="checkbox">
+              <a class="pull-right" style="cursor: pointer" data-dismiss="modal" data-target="#forgotpasswordModal"
+                data-toggle="modal">
+                Forgot Password?
+              </a>
+            </div>
+
+          </div>
+          <div class="modal-footer">
+            <input class="btn btn-primary" name="login" type="submit" value="Login">
+            <button type="button" class="btn btn-dark" data-dismiss="modal">
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </form>
+
+  <form method="post" id="loginformDriver">
+    <div class="modal" id="loginModalDriver" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h4 id="myModalLabel">
+              Driver Login
+            </h4>
+          </div>
+          <div class="modal-body">
+            <div id="loginmessagedriver"></div>
+            <div class="form-group">
+              <label for="loginemail" class="sr-only">Email:</label>
+              <input class="form-control" type="email" name="loginemail" id="loginemail" placeholder="Email"
+                maxlength="50">
+            </div>
+            <div class="form-group">
+              <label for="loginpassword" class="sr-only">Password</label>
+              <input class="form-control" type="password" name="loginpassword" id="loginpassword" placeholder="Password"
+                maxlength="30">
+            </div>
+            <div class="checkbox">
+              <a class="pull-right" style="cursor: pointer" data-dismiss="modal"
+                data-target="#forgotpasswordModalDriver" data-toggle="modal">
+                Forgot Password?
+              </a>
+            </div>
+
+          </div>
+          <div class="modal-footer">
+            <input class="btn btn-primary" name="login" type="submit" value="Login">
+            <button type="button" class="btn btn-dark" data-dismiss="modal">
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </form>
+
+  <form method="post" id="signupform">
+    <div class="modal" id="signupModalRider" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+
+            <h4 id="myModalLabel">
+              Sign up and join your first ride!
+            </h4>
+          </div>
+          <div class="modal-body">
+            <div id="signupmessage"></div>
+
+            <div class="form-group">
+              <label for="username" class="sr-only">Username:</label>
+              <input class="form-control" type="text" name="username" id="username" placeholder="Username"
+                maxlength="30">
+            </div>
+            <div class="form-group">
+              <label for="email" class="sr-only">Email:</label>
+              <input class="form-control" type="email" name="email" id="email" placeholder="Email Address"
+                maxlength="50">
+            </div>
+            <div class="form-group">
+              <label for="password" class="sr-only">Choose a password:</label>
+              <input class="form-control" type="password" name="password" id="password" placeholder="Choose a password"
+                maxlength="30">
+            </div>
+            <div class="form-group">
+              <label for="password2" class="sr-only">Confirm password</label>
+              <input class="form-control" type="password" name="password2" id="password2" placeholder="Confirm password"
+                maxlength="30">
+            </div>
+          </div>
+          <div class="modal-footer">
+            <input class="btn btn-primary" name="signup" type="submit" value="Sign up">
+            <button type="button" class="btn btn-dark" data-dismiss="modal">
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </form>
+
+  <form method="post" id="signupformDriver">
+    <div class="modal" id="signupModalDriver" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h4 id="myModalLabel">
+              Sign up and create your first ride!
+            </h4>
+          </div>
+          <div class="modal-body">
+            <div id="signupmessagedriver"></div>
+
+            <div class="form-group">
+              <label for="username" class="sr-only">Username:</label>
+              <input class="form-control" type="text" name="username" id="username" placeholder="Username"
+                maxlength="30">
+            </div>
+            <div class="form-group">
+              <label for="email" class="sr-only">Email:</label>
+              <input class="form-control" type="email" name="email" id="email" placeholder="Email Address"
+                maxlength="50">
+            </div>
+            <div class="form-group">
+              <label for="password" class="sr-only">Choose a password:</label>
+              <input class="form-control" type="password" name="password" id="password" placeholder="Choose a password"
+                maxlength="30">
+            </div>
+            <div class="form-group">
+              <label for="password2" class="sr-only">Confirm password:</label>
+              <input class="form-control" type="password" name="password2" id="password2" placeholder="Confirm password"
+                maxlength="30">
+            </div>
+            <div class="form-group">
+              <label for="carMake" class="sr-only">Car Make:</label>
+              <input class="form-control" type="text" name="carMake" id="carMake" placeholder="Enter Car Make"
+                maxlength="30">
+            </div>
+            <div class="form-group">
+              <label for="carModel" class="sr-only">Car Model:</label>
+              <input class="form-control" type="text" name="carModel" id="carModel" placeholder="Enter Car Model"
+                maxlength="30">
+            </div>
+            <div class="form-group">
+              <label for="seats" class="sr-only">Seating Capacity:</label>
+              <input class="form-control" type="text" name="seats" id="seats" placeholder="Enter seating capacity"
+                maxlength="30">
+            </div>
+          </div>
+          <div class="modal-footer">
+            <input class="btn btn-primary" name="signup" type="submit" value="Sign up">
+            <button type="button" class="btn btn-dark" data-dismiss="modal">
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </form>
+
+
+  <form method="post" id="forgotpasswordform">
+    <div class="modal" id="forgotpasswordModal" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+
+            <h4 id="myModalLabel">
+              Forgot Password? Enter your email address:
+            </h4>
+          </div>
+          <div class="modal-body">
+
+
+            <div id="forgotpasswordmessage"></div>
+
+
+            <div class="form-group">
+              <label for="forgotemail" class="sr-only">Email:</label>
+              <input class="form-control" type="email" name="forgotemail" id="forgotemail" placeholder="Email"
+                maxlength="50">
+            </div>
+          </div>
+          <div class="modal-footer">
+            <input class="btn btn-primary" name="forgotpassword" type="submit" value="Submit">
+            <button type="button" class="btn btn-dark" data-dismiss="modal">
+              Cancel
+            </button>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </form>
+
+
+
+  <form method="post" id="forgotpasswordformdriver">
+    <div class="modal" id="forgotpasswordModalDriver" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+
+            <h4 id="myModalLabel">
+              Forgot Password? Enter your email address:
+            </h4>
+          </div>
+          <div class="modal-body">
+
+
+            <div id="forgotpasswordmessagedriver"></div>
+
+
+            <div class="form-group">
+              <label for="forgotemail" class="sr-only">Email:</label>
+              <input class="form-control" type="email" name="forgotemail" id="forgotemail" placeholder="Email"
+                maxlength="50">
+            </div>
+          </div>
+          <div class="modal-footer">
+            <input class="btn btn-primary" name="forgotpassword" type="submit" value="Submit">
+            <button type="button" class="btn btn-dark" data-dismiss="modal">
+              Cancel
+            </button>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </form>
+    
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
 </body>
