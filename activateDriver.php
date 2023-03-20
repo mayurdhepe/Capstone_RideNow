714546472519f42d28954504204c538b0c18ebb4
Mayur Dhepe <mayurdhepe@gmail.com>
2023-03-20 01:28:35 -0400

    added styles for activate driver

--- activateDriver.php
+++ activateDriver.php
@@ -28,9 +28,52 @@ include('driverDao.php');
 </head>
 
 <body>
-    <div class="container-fluid">
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">RideNow</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="about.php">About</a>
+            </li>
+            <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
+                Login
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a href="#loginModal" class="dropdown-item" data-toggle="modal">Rider Login</a>
+                <a href="#loginModalDriver" class="dropdown-item" data-toggle="modal">Driver Login</a>
+            </div>
+            </li>
+
+            <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown2" role="button" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
+                Sign Up
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown2">
+                <a href="#signupModalRider" class="dropdown-item" data-toggle="modal">Rider Sign Up</a>
+                <a href="#signupModalDriver" class="dropdown-item" data-toggle="modal">Driver Sign Up</a>
+            </div>
+            </li>
+
+        </ul>
+        
+        </div>
+  </nav>
+    <div class="container-fluid" style="margin-left: 10rem; margin-top:3rem;" >
         <div class="row">
-            <div class="col-sm-offset-1 col-sm-10 contactForm">
+            <div class="col-sm-offset-1 col-sm-10 contactForm" style="border: none;">
                 <h1>Account Activation</h1>
                 <?php
                 if (!isset($_GET['email']) || !isset($_GET['key'])) {
