5c92d1330b5e38e852a2eec4b565e6329694fe7f
Aman Saraf <amansaraf28@gmail.com>
2023-04-27 20:34:57 -0400

    StartRide

--- startride.php
+++ startride.php
@@ -4,6 +4,14 @@ include('dbConnect.php');
 $sql = "UPDATE Rides SET status = 1 WHERE ride_id='" . $_POST['ride_id'] . "'";
 $result = mysqli_query($link, $sql);
 
+$sql = "SELECT email from users WHERE user_id IN (SELECT rider_id FROM RideRiders WHERE ride_id='" . $_POST['ride_id'] . "')";
+$result = mysqli_query($link, $sql);
+while ($row = mysqli_fetch_array($result)) {
+    $email = $row['email'];
+    $message = "Your Ride has started! Happy journey!";
+    mail($email, 'Ride Started', $message, 'From:' . 'gradproj@grad-project.host20.uk');
+}
+
 //email riders
 
 ?>
